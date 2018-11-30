4bc04529b767657a697468e42c2a8e96b4950729
Maziyar Gerami <maziyar.grami@gmail.com>
2018-11-30 11:12:53 +0330

    Commit for ./others/RSA/Coding/main.m

---
+++ others/RSA/Coding/main.m
@@ -0,0 +1,32 @@
+clc
+clear
+close all
+%% define parameters
+
+output = '..\Files\codeddata.mat';
+
+
+%% read files
+input = input('file : ');
+input = fopen(input,'r');
+input = fscanf(input, '%c')
+
+%% STEPS
+
+% start timming with tic
+tic
+
+% STEP 1: create coded data with RSA: input of RSA algorithm is input data
+[coded_data, ~] = RSA(input);
+
+
+% end timming
+toc
+
+%% write to disk
+
+% Final: write it to a file for transfer
+
+save(output, 'coded_data');
+
+
