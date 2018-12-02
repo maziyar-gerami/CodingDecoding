a386aa9258f3d6c50feb7f5e41fa6b787792929c
Maziyar Gerami <maziyar.grami@gmail.com>
2018-12-02 17:01:46 +0330

    Commit for ./others/AES/Decoding/main.m

---
+++ others/AES/Decoding/main.m
@@ -0,0 +1,27 @@
+clc
+clear
+close all
+%% define parameters
+
+input = '..\Files\codeddata.mat';
+
+
+
+%% read files
+
+load (input);
+
+%% STEPS
+
+% start timming with tic
+tic
+
+
+% STEP 1: create coded data with RSA: input of RSA algorithm is input data
+decoded_data = cipher(coded_data,'DEC', key);
+
+% end timming
+toc
+
+
+
