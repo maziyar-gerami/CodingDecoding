f6ee42d42bb5866e0ed502fa772fec0f9de59844
Maziyar Gerami <maziyar.grami@gmail.com>
2018-12-02 17:00:06 +0330

    Commit for ./others/AES/Coding/main.m

---
+++ others/AES/Coding/main.m
@@ -0,0 +1,33 @@
+clc
+clear
+close all
+%% define parameters
+
+output = '..\Files\codeddata.mat';
+
+
+%% read files
+input = input('file : ');
+input = fopen(input,'r');
+input = fscanf(input, '%c')
+
+%% STEPS
+
+% start timming with tic
+tic
+
+input = hexToBinaryVector(input , 64);
+
+% STEP 1: create coded data with RSA: input of RSA algorithm is input data
+[coded_data, key] = cipher(input,'ENC');
+
+% end timming
+toc
+
+%% write to disk
+
+% Final: write it to a file for transfer
+
+save(output, 'coded_data', 'key');
+
+
